988e9763b2db9ca58bc3999f4f32901a2a2d59a1
TickTockBent <benttick@gmail.com>
2022-05-07 14:32:27 -0400

    Initial player camera/movement

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public float sensitivityX;
+    public float sensitivityY;
+
+    public Transform orientation;
+
+    private float xRotation;
+    private float yRotation;
+    
     void Start()
     {
-        
+        //lock cursor to middle of screen, hide cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
+        //get mouse input
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensitivityX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensitivityY;
+
+        yRotation += mouseX;
         
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        
+        //rotate camera and orientation
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
     }
 }
